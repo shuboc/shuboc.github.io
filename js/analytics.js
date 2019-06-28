f833b0532fc26e4d65cba164ffd180002f6a1637
Shubo Chao <apiggy0204@gmail.com>
2019-06-28 12:23:04 +0800

    Track article viewing events

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+const EVENT_LABEL_HOME = 'home';
+const EVENT_LABEL_TAGS = 'tags'
+const EVENT_LABEL_HOME_PAGINATION = 'home_pagination';
+const EVENT_LABEL_RELATED_ARTICLES = 'related_articles';
+const EVENT_LABEL_LATEST_ARTICLES = 'latest_articles';
+const EVENT_LABEL_ARTICLES_BY_TAG = 'articles_by_tag';
+
+function trackViewArticle(event_label) {
+  gtag('event', 'view_item', { event_label });
+}
+
+function trackViewArticleList(event_label) {
+  gtag('event', 'view_item_list', { event_label });
+}
