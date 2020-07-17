55fdfcc014e1e9e079c80955d1bdbe47be527fce
Shubo Chao <shubochao@gmail.com>
2020-07-17 16:57:37 +0800

    Adjust layout
    - Remove latest posts
    - Add trending posts and recommend books

--- js/analytics.js
+++ js/analytics.js
@@ -4,12 +4,14 @@ const EVENT_LABEL_HOME = 'home';
 const EVENT_LABEL_TAGS = 'tags'
 const EVENT_LABEL_HOME_PAGINATION = 'home_pagination';
 const EVENT_LABEL_RELATED_ARTICLES = 'related_articles';
+const EVENT_LABEL_TRENDING_ARTICLES = 'trending_articles';
+const EVENT_LABEL_RECOMMEND_BOOKS = 'recommend_books';
 const EVENT_LABEL_LATEST_ARTICLES = 'latest_articles';
 const EVENT_LABEL_ARTICLES_BY_TAG = 'articles_by_tag';
 const EVENT_LABEL_ARTICLES_BY_TAG_MORE = 'articles_by_tag_more';
 
-function trackViewArticle(event_label) {
-  gtag('event', 'view_item', { event_label });
+function trackViewArticle(event_label, items) {
+  gtag('event', 'view_item', { event_label, items });
 }
 
 function trackViewArticleList(event_label) {
